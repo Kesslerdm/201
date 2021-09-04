92b80b6afffce8aaf302aeea9820e52c553448fe
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-09-04 16:57:45 -0400

    Update load.js

--- starter/load.js
+++ starter/load.js
@@ -1,5 +1,6 @@
-const starter = require('./main');
+const starter = require("../main");
 const base = Buffer.alloc(1, 0);
+const http = require("http");
 
 /**
  * @param {http.IncomingMessage} req
@@ -7,37 +8,54 @@ const base = Buffer.alloc(1, 0);
  * @param {import("url").UrlWithParsedQuery} url
  * @returns {boolean}
  */
-
 module.exports = function (req, res, url) {
 	switch (req.method) {
-		case 'GET': {
+		case "GET": {
 			const match = req.url.match(/\/starters\/([^.]+)(?:\.(zip|xml))?$/);
 			if (!match) return;
 
-			var id = match[1], ext = match[2];
+			var id = match[1];
+			var ext = match[2];
 			switch (ext) {
-				case 'zip':
-					res.setHeader('Content-Type', 'application/zip');
-					starter.loadZip(id).then(v => { res.statusCode = 200, res.end(v) })
-						.catch(e => { res.statusCode = 404, res.end() })
+				case "zip":
+					res.setHeader("Content-Type", "application/zip");
+					movie.loadZip(id).then((v) => {
+						if (v) {
+							res.statusCode = 200;
+							res.end(v);
+						} else {
+							res.statusCode = 404;
+							res.end();
+						}
+					});
 					break;
 				default:
-					res.setHeader('Content-Type', 'text/xml');
-					starter.loadXml(id).then(v => { res.statusCode = 200, res.end(v) })
-						.catch(e => { res.statusCode = 404, res.end() })
+					res.setHeader("Content-Type", "text/xml");
+					starter.loadXml(id).then((v) => {
+						if (v) {
+							res.statusCode = 200;
+							res.end(v);
+						} else {
+							res.statusCode = 404;
+							res.end();
+						}
+					});
+					break;
 			}
 			return true;
 		}
 
-		case 'POST': {
-			if (!url.path.startsWith('/goapi/getMovie/?')) return;
-			res.setHeader('Content-Type', 'application/zip');
+		case "POST": {
+			if (!url.path.startsWith("/goapi/getMovie/?")) return;
+			res.setHeader("Content-Type", "application/zip");
 
-			starter.loadZip(url.query.starterId).then(b =>
-				res.end(Buffer.concat([base, b]))
-			).catch(e => res.end('1'));
+			starter
+				.loadZip(url.query.starterId)
+				.then((b) => res.end(Buffer.concat([base, b])))
+				.catch(() => res.end("1"));
 			return true;
 		}
-		default: return;
+		default:
+			return;
 	}
-}
+};
