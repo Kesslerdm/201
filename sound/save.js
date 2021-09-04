27f566e2b35aab061453b2db37740b41e8288b7a
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-09-03 22:48:54 -0400

    Update save.js

--- sound/save.js
+++ sound/save.js
@@ -11,8 +11,11 @@ const http = require("http");
 module.exports = function (req, res, url) {
 	if (req.method != "POST" || (url.path != "/goapi/saveSound/")) return;
 	loadPost(req, res).then(([data, mId]) => {
-				var bytes = Buffer.from(data.bytes, "base64");
-				asset.save(bytes, mId, "voiceover", "ogg");
-			});
+		const trigAutosave = data.is_triggered_by_autosave;
+		if (trigAutosave && (!data.assetId || data.noAutosave)) return res.end("0");
+
+		var bytes = Buffer.from(data.bytes, "base64");
+		sound.save(body, mId, data.presaveId).then((nId) => res.end("0" + nId));
+	});
 	return true;
 };
