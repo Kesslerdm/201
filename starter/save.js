7a93a9ddc7784ca4a3fd18b03b527a0e626f45da
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-09-05 00:02:52 -0400

    Update save.js

--- starter/save.js
+++ starter/save.js
@@ -1,17 +1,22 @@
-const loadPost = require('../request/post_body');
-const starter = require('../main');
+const loadPost = require("../request/post_body");
+const starter = require("../main");
+const http = require("http");
 
+/**
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ * @param {import("url").UrlWithParsedQuery} url
+ * @returns {boolean}
+ */
 module.exports = function (req, res, url) {
-	if (req.method != 'POST' || url.path != '/goapi/saveTemplate/') return;
-	loadPost(req, res).then(data => {
-
+	if (req.method != "POST" || (url.path != "/goapi/saveTemplate/")) return;
+	loadPost(req, res).then(([data, mId]) => {
 		const trigAutosave = data.is_triggered_by_autosave;
-		if (trigAutosave && (!data.starterId || data.noAutosave))
-			return res.end('0');
+		if (trigAutosave && (!data.starterId || data.noAutosave)) return res.end("0");
 
-		var body = Buffer.from(data.body_zip, 'base64');
-		var thumb = data.thumbnail_large && Buffer.from(data.thumbnail_large, 'base64');
-		starter.save(body, thumb, data.starterId || data.presaveId, data.presaveId).then(nId => res.end('0' + nId));
+		var body = Buffer.from(data.body_zip, "base64");
+		var thumb = data.thumbnail_large && Buffer.from(data.thumbnail_large, "base64");
+		starter.save(body, thumb, mId, data.presaveId).then((nId) => res.end("0" + nId));
 	});
 	return true;
-}
+};
