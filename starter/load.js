a23cc597e77cd0ed6fab5217afb57c686d0faff9
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-09-04 22:42:00 -0400

    Update load.js

--- starter/load.js
+++ starter/load.js
@@ -1,61 +1,31 @@
-const starter = require("../main");
+const starter = require('../main');
 const base = Buffer.alloc(1, 0);
-const http = require("http");
 
-/**
- * @param {http.IncomingMessage} req
- * @param {http.ServerResponse} res
- * @param {import("url").UrlWithParsedQuery} url
- * @returns {boolean}
- */
 module.exports = function (req, res, url) {
 	switch (req.method) {
-		case "GET": {
-			const match = req.url.match(/\/starters\/([^.]+)(?:\.(zip|xml))?$/);
+		case 'GET': {
+			const match = req.url.match(/\/starters\/([^.]+)(?:\.(xml))?$/);
 			if (!match) return;
 
-			var id = match[1];
-			var ext = match[2];
+			var id = match[1], ext = match[2];
 			switch (ext) {
-				case "zip":
-					res.setHeader("Content-Type", "application/zip");
-					movie.loadZip(id).then((v) => {
-						if (v) {
-							res.statusCode = 200;
-							res.end(v);
-						} else {
-							res.statusCode = 404;
-							res.end();
-						}
-					});
-					break;
-				default:
-					res.setHeader("Content-Type", "text/xml");
-					starter.loadXml(id).then((v) => {
-						if (v) {
-							res.statusCode = 200;
-							res.end(v);
-						} else {
-							res.statusCode = 404;
-							res.end();
-						}
-					});
-					break;
+				case 'xml':
+					res.setHeader('Content-Type', 'text/xml');
+					starter.loadXml(id).then(v => { res.statusCode = 200, res.end(v) })
+						.catch(e => { res.statusCode = 404, res.end() })
 			}
 			return true;
 		}
 
-		case "POST": {
-			if (!url.path.startsWith("/goapi/getMovie/?")) return;
-			res.setHeader("Content-Type", "application/zip");
+		case 'POST': {
+			if (!url.path.startsWith('/goapi/getTemplate/')) return;
+			res.setHeader('Content-Type', 'application/zip');
 
-			starter
-				.loadZip(url.query.starterId)
-				.then((b) => res.end(Buffer.concat([base, b])))
-				.catch(() => res.end("1"));
+			starter.loadXml(url.query.starterId).then(b =>
+				res.end(Buffer.concat([base, b]))
+			).catch(e => res.end('1'));
 			return true;
 		}
-		default:
-			return;
+		default: return;
 	}
-};
+}
