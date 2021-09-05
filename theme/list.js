04a19f47cd5fcccbe41eb262291c011421ff244d
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-09-04 23:02:55 -0400

    Update list.js

--- theme/list.js
+++ theme/list.js
@@ -33,11 +33,19 @@
 "willie"
 */
 
-const fUtil = require('../fileUtil');
+const http = require("http");
+const fUtil = require("../fileUtil");
 const folder = process.env.THEME_FOLDER;
+
+/**
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ * @param {import("url").UrlWithParsedQuery} url
+ * @returns {boolean}
+ */
 module.exports = function (req, res, url) {
-	if (req.method != 'POST' || url.path != '/goapi/getThemeList/') return;
-	res.setHeader('Content-Type', 'application/zip');
-	fUtil.zippy(`${folder}/themelist.xml`, 'themelist.xml').then(b => res.end(b));
+	if (req.method != "POST" || url.path != "/goapi/getThemeList/") return;
+	res.setHeader("Content-Type", "application/zip");
+	fUtil.makeZip(`${folder}/themelist.xml`, "themelist.xml").then((b) => res.end(b));
 	return true;
-}
+};
