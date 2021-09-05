2018c5635fe932fa5e2f93eda450a63f7ccd3b7b
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-09-04 22:44:11 -0400

    Update load.js

--- starter/load.js
+++ starter/load.js
@@ -4,14 +4,14 @@ const base = Buffer.alloc(1, 0);
 module.exports = function (req, res, url) {
 	switch (req.method) {
 		case 'GET': {
-			const match = req.url.match(/\/starters\/([^.]+)(?:\.(xml))?$/);
+			const match = req.url.match(/\/starters\/([^.]+)(?:\.(png))?$/);
 			if (!match) return;
 
 			var id = match[1], ext = match[2];
 			switch (ext) {
-				case 'xml':
-					res.setHeader('Content-Type', 'text/xml');
-					starter.loadXml(id).then(v => { res.statusCode = 200, res.end(v) })
+				case 'png':
+					res.setHeader('Content-Type', 'image/png');
+					starter.loadPng(id).then(v => { res.statusCode = 200, res.end(v) })
 						.catch(e => { res.statusCode = 404, res.end() })
 			}
 			return true;
@@ -21,7 +21,7 @@ module.exports = function (req, res, url) {
 			if (!url.path.startsWith('/goapi/getTemplate/')) return;
 			res.setHeader('Content-Type', 'application/zip');
 
-			starter.loadXml(url.query.starterId).then(b =>
+			starter.loadPng(url.query.starterId).then(b =>
 				res.end(Buffer.concat([base, b]))
 			).catch(e => res.end('1'));
 			return true;
